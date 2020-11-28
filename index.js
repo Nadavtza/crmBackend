be9fc534084167293f018bf76a7574154593ca31
Nadav <nadavtz16@gmail.com>
2020-11-28 16:31:32 +0200

    Added static files support

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ app.use(bodyParser.json());
 
 routes(app);
 
+// serving static files
+app.use(express.static('pablic'));
+
 app.get('/', (req , res) => {
     res.send(`Node and express server running on port ${PORT}`);
 });
