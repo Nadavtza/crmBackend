9b87f695c8c2afc758eaa3a22e911da08d49214d
Nadav <nadavtz16@gmail.com>
2020-11-28 16:26:16 +0200

    Added CRUD operations with MongoDB

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,15 +1,25 @@
+import { addNewContact, getContacts, getContactByID, upateContact, deleteContact } from '../controllers/crmController'
+
 const routes = (app) => {
   app
     .route("/contact")
-    .get((req, res) => res.send("GET request successful!"))
+    .get((req, res, next) => {
+        // middleware
+        console.log(`Request from: ${req.originalUrl}`);
+        console.log(`Request from: ${req.method}`);
+        next();
+    }, getContacts)
 
-    .post((req, res) => res.send("Post request successful!"));
+    .post(addNewContact);
 
   app
     .route("/contact/:contactID")
-    .put((req, res) => res.send("PUT request successful!"))
 
-    .delete((req, res) => res.send("DELETE request successful!"));
+    .get(getContactByID)
+
+    .put(upateContact)
+
+    .delete(deleteContact);
 };
 
 
